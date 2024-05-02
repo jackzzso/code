5b71f19c823bbecaf850aabdcd7d74a1c77122af
jackzzso <168654624+jackzzso@users.noreply.github.com>
2024-05-02 09:34:26 -0600

    Update camera.cs

--- camera.cs
+++ camera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// camera system
 public class CameraFollower : MonoBehavior {
     public static CameraFollower instance;
 
