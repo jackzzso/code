67790c415cff1bf82aba1b5235ffd4fe77cc41d2
jackzzso <168654624+jackzzso@users.noreply.github.com>
2024-05-02 08:14:43 -0600

    Update camera.cs

--- camera.cs
+++ camera.cs
@@ -1,1 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehavior {
+    public static CameraFollower instance;
+
+    public float MovementSmoothness = 1f;
+    public float RotationSmoothness = 1f;
+    public GameObject FollowTarget;
+    public bool CanFollow = true;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start() 
+    {
+
+    }
+    void LateUpdate()
+    {
+        if(FollowTarget == null || !canFollow)
+            return;
+        transform.position = Vector3.Lerp(transform.position,FollowTarget.transform.position,Time.deltaTIme*MovementSmoothness);
+        transform.rotation - Quaternion.Slerp(transform.rotation,FollowTarget.transform.rotation,Time.deltaTime*RotationSmoothness);
+    }
+}
 
