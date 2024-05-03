40703f7a265fe8b3182811c492ed0cf1ba09b888
jackzzso <168654624+jackzzso@users.noreply.github.com>
2024-05-03 12:42:58 -0600

    Update patrol.cs

--- patrol.cs
+++ patrol.cs
@@ -1,1 +1,50 @@
-// adding later
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeetlePatrol : MonoBehaviour {
+	public static bool isDie, isEating, isAttacking = false;
+
+	public float speed = 5;
+	public float directionChangeInterval = 1;
+	public float maxHeadingChange = 30;
+
+	Animator beetleAnim;
+
+	CharacterController controller;
+	float heading;
+	Vector3 targetRotation;
+
+	void start() {
+		controller = GetComponent<CharacterController>();
+		beetleAnim = GetComponent<Animator>();
+
+		heading = Random.Range(0, 360);
+		transform.eulerAngles = new Vector3(0, heading, 0);
+
+		StartCoroutine(NewHeading());
+	}
+
+	void Update () 
+	{
+		if ((!isDie) & (!isEating) & (!isAttacking)) {
+			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+			var forward = transform.TransformDirection(Vector3.forward);
+			controller.SimpleMove(forward * speed);
+		}
+	}
+	IEnumerator NewHeading()
+	{
+		while (true) {
+			NewHeadingRoutine();
+			yield return new WaitForSeconds(directionChangeInterval);
+		}
+	}
+	void NewHeadingRoutine()
+	{
+		var floor = transform.eulerAngles.y - maxHeadingChange;
+		var ceil = transform.eulerAngles.y + maxHeadingChange;
+		heading = Random.Range(floor, ceil);
+		targetRotation = new Vector3(0, heading, 0);
+	}
+}
